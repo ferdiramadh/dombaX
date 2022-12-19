ca449d957e094d1f3c60fd5d29fb7c355ec35493
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2022-12-20 00:04:58 +0700

    Update on Image Picker.

--- utils/ImageUpload.js
+++ utils/ImageUpload.js
@@ -1,9 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
 import firebase from '../Firebaseconfig'
-import { useState } from 'react';
-
-// export const [ dwnldUrl, setDwnldUrl ] = useState()
 
 export const storeImgData = async (image) => {
     try {
@@ -88,9 +85,9 @@ export const storeImgData = async (image) => {
 
     
 
-    if (!result.cancelled) {
-      // setImage(result.uri);
-      uploadImage(result.uri, storageFolder, id, setLoad, collection, setPercent);
+    if (!result.canceled) {
+      // setImage(result.assets[0].uri);
+      uploadImage(result.assets[0].uri, storageFolder, id, setLoad, collection, setPercent);
       
       
     }
@@ -111,8 +108,8 @@ export const storeImgData = async (image) => {
 
   //   console.log(result);
 
-  //   if (!result.cancelled) {
-  //       setFunc(result.uri);
+  //   if (!result.canceled) {
+  //       setFunc(result.assets[0].uri);
   //   }
   // };
 
@@ -129,8 +126,8 @@ export const storeImgData = async (image) => {
 
     console.log(result);
 
-    if (!result.cancelled) {
-      uploadImage(result.uri, storageFolder, id, setLoad, collection, setPercent);
+    if (!result.canceled) {
+      uploadImage(result.assets[0].uri, storageFolder, id, setLoad, collection, setPercent);
     }
   };
 
@@ -145,14 +142,15 @@ export const storeImgData = async (image) => {
       maxHeight: 500,
     });
 
-    console.log(result);
+    // console.log(result);
 
-    if (!result.cancelled && isUpdate) {
-      setFunc('image', result.uri);
+    if (!result.canceled && isUpdate) {
+      setFunc('image', result.assets[0].uri);
+      // console.log(result.assets[0].uri);
     }
 
-    if (!result.cancelled) {
-      setFunc(result.uri);
+    if (!result.canceled) {
+      setFunc(result.assets[0].uri);
     }
   };
 
@@ -172,23 +170,7 @@ export const storeImgData = async (image) => {
     }
   }
 
-  // export const updateImageDoc = (collection, itemId, downloadUrl) => {
-  //   if(downloadUrl){
-  //     return firebase
-  //     .firestore()
-  //     .collection(collection)
-  //     .doc(itemId)
-  //     .update({
-  //       "image": downloadUrl
-  //     }).then(() => {
-  //       console.log('Item Updated')
-  //     }).catch((error) => console.log(error))
-  //   } else {
-  //     console.log("tidak ada yeuh")
-  //   }
-  // }
   
-
   export const uploadImage = async (image, storageFolder, id, setLoad, collection, setPercent) => {
     const blob = await new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
@@ -302,79 +284,3 @@ export const storeImgData = async (image) => {
       deleteFile(storageFolder, tempObj)
     }
   }
-
-
-  // const uploadImage = async () => {
-  //   const blob = await new Promise((resolve, reject) => {
-  //     const xhr = new XMLHttpRequest();
-  //     xhr.onload = function() {
-  //       resolve(xhr.response);
-  //     };
-  //     xhr.onerror = function() {
-  //       reject(new TypeError('Network request failed'));
-  //     };
-  //     xhr.responseType = 'blob';
-  //     xhr.open('GET', image, true);
-  //     xhr.send(null);
-  //   });
-
-  //   const ref = firebase.storage().ref().child(new Date().toISOString())
-  //   const snapshot = ref.put(blob)
-
-  //   snapshot.on(firebase.storage.TaskEvent.STATE_CHANGED),
-  //   snapshot => {
-  //           console.log("Sukses")
-  //           console.log(snapshot)
-  //           console.log("snapshot" + snapshot.state)
-  //           console.log("PROGRES" + snapshot.bytesTransferred/snapshot.totalBytes * 100)
-  
-  //           // if(snapshot.state === firebase.storage.TaskEvent.SUCCESS){
-  //           //   console.log("Sukses")
-  //           // }
-  //         },
-  //   error => {
-  //     // unsubscribe()
-  //     console.log("error wak" + error)
-  //   },
-  //   () => {
-  //     snapshot.snapshot.ref.getDownloadURL()
-  //     .then((downloadUrl) => {
-  //       console.log("File available at" + downloadUrl)
-  //     })
-  //   }
- 
-  //   // if(image != null) {
-  //   //   const fileExtension = image.split('.').pop();
-  //   //   console.log("FILE EXT:" + fileExtension)
-
-  //   //   var imgId = "1"
-  //   //   const fileName = `${imgId}.${fileExtension}`
-  //   //   console.log(fileName)
-
-  //   //   var storageRef = firebase.storage().ref.child(`food/images/${fileName}`)
-  //   //   storageRef.put(image)
-  //   //   .on(
-  //   //     firebase.storage.TaskEvent.STATE_CHANGED,
-  //   //     snapshot => {
-  //   //       console.log("Sukses")
-  //   //       console.log(snapshot)
-  //   //       // console.log("snapshot" + snapshot.state)
-  //   //       // console.log("PROGRES" + snapshot.bytesTransferred/snapshot.totalBytes * 100)
-
-  //   //       // if(snapshot.state === firebase.storage.TaskEvent.SUCCESS){
-  //   //       //   console.log("Sukses")
-  //   //       // }
-  //   //     },
-  //   //     error => {
-  //   //       unsubscribe()
-  //   //       console.log("error wak" + error)
-  //   //     },
-  //   //     () => {
-  //   //       storageRef.getDownloadURL()
-  //   //       .then((downloadUrl) => {
-  //   //         console.log("File available at" + downloadUrl)
-  //   //       })
-  //   //     }
-  //   //   )
-  //   // }
-  // }
