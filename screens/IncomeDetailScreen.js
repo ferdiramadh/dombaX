7addc6b52e90cdf9e4e27114bad4196009ab0b53
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-04-02 04:33:12 +0700

    Remove delete functions inside Rincian Transaksi screen.

--- screens/IncomeDetailScreen.js
+++ screens/IncomeDetailScreen.js
@@ -1,82 +1,15 @@
-import { StyleSheet, Text, View , ActivityIndicator, Alert, TouchableOpacity, TextInput, ScrollView, Image} from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View , Image} from 'react-native'
+import React, { useState } from 'react'
 import { windowWidth } from '../utils/DimensionSetup';
-import { MaterialIcons } from '@expo/vector-icons';
-import { deleteFile, deleteCollection } from '../utils/ImageUpload';
 import IncomeDetails from '../components/income/IncomeDetails';
 import ExpenseDetails from '../components/expense/ExpenseDetails';
 
 const IncomeDetailScreen = ({ route }) => {
-  const { editData, navigation, isSearch, searchItems, setSearchItems, isExpense } = route.params;
+  const { editData, navigation, isExpense } = route.params;
   const [isUpdate, setIsUpdate] = useState(false)
-
-  const [ database, setDatabase ] = useState({
-    collection: '',
-    storage: ''
-  })
-
-
-  const deleteItem = (item) => {
-    Alert.alert(
-        "Perhatian!",
-        `Hapus item?`,
-        [
-            {
-                text:"Batal",
-                onPress: () => Alert.alert("Dibatalkan."),
-                style:'cancel'
-            },
-            {
-                text: "OK",
-                onPress: () => {
-                  deleteCollectionAndFile(item)
-                  navigation.navigate("Transaction")
-                }
-            }
-        ],
-        {
-            cancelable: true,
-            
-        }
-    )
-    
-    
-}
-
-const deleteCollectionAndFile = (editData) => {
-    deleteCollection(database.collection, editData)
-    deleteFile(database.storage, editData)
-}
-
-useEffect(() => {
-  if(isExpense) {
-    setDatabase({
-      collection: 'expense',
-      storage: 'Expense'
-    })
-  } else {
-    setDatabase({
-      collection: 'income',
-      storage: 'Income'
-    })
-  }
-}, [editData])
 
   return (
     <View style={styles.container}>
-      {!isUpdate? 
-      <TouchableOpacity style={{marginLeft:10, position: 'absolute', top: 10, right: 10}} onPress={() => {
-        if(isSearch) {
-          let tempList = searchItems.filter(({id}) => id !== editData.id)
-          console.log("coba set newitems")
-          setSearchItems(tempList)
-          deleteItem(editData)      
-        } else {
-          deleteItem(editData)
-        }
-      }}>
-          <MaterialIcons name="delete" size={24} color="black" />
-      </TouchableOpacity>: null}
       {!isUpdate? 
       <View style={styles.iconContainer}>
         <Image source={require('../assets/Gembul.png')} style={styles.img} resizeMode='contain'/>
