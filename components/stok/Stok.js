09a5d5a8c069a57efdf0071ccbe2a245129a0c77
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-05-27 17:10:31 +0700

    GEMBUL-44 Add image for empty state on Stock and some clean up.

--- components/stok/Stok.js
+++ components/stok/Stok.js
@@ -1,45 +1,23 @@
 import React from 'react'
-import { StyleSheet, Text, View,TouchableOpacity , ScrollView} from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import DombaStokSection from './domba/DombaStokSection'
-import PakanStokSection from './pakan/PakanStokSection'
-import ObatStokSection from './obatdansuplemen/ObatStokSection'
 import { useSelector} from 'react-redux'
 
 const Stok = () => {
-    const stokState = useSelector(state => state.stokReducer)
+
     const userProducts = useSelector(state => state.userProductReducer);
     const DATA = userProducts.listUserProduct
-    // const DATA = stokState.listDomba
-    const DATA_PAKAN = stokState.listPakan
-    const DATA_OBAT = stokState.listObat
-    
+    const imgEmptyState = require('../../assets/images/stock/empty_stock.png')
 
-    const sectionShow = () => {
-        return(
-            <View>
-                {
-                    DATA.length !== 0 ? <DombaStokSection />: null
-                  
-                }
-                {
-                    DATA_PAKAN.length !== 0 ? <PakanStokSection />:null
-                }
-                {
-                    DATA_OBAT.length !== 0 ? <ObatStokSection />:null
-                }
-            </View>
-        )
-        
-    }
     return (
         <View style={styles.container}>
             <View style={styles.insideContainer}>
             { DATA.length == 0 ?
             <View style={styles.emptyStokNotif}>
+                <Image source={imgEmptyState} style={styles.img}/>
                 <Text style={styles.text}>Stok Kamu masih kosong, silahkan tekan <Text style={{fontWeight:'bold'}}>tombol tambah</Text> untuk menambahkan produk </Text>
-            </View>:
+            </View> :
             <DombaStokSection /> }      
-            
             </View>
         </View>
     )
@@ -49,33 +27,32 @@ export default Stok
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
-        // justifyContent:'center',
-        // alignItems:'center',
-        // padding:10,
-        // backgroundColor:'purple',
-        width:'100%',
-       
+        flex: 1,
+        width: '100%',   
     },
     insideContainer:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        // backgroundColor:'cyan',
-        width:'100%',
-       
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
     },
     text:{
-        fontSize: 20,
-        fontWeight:'500',
-        textAlign:'center',
-        fontFamily:'Inter'
+        fontSize: 15,
+        fontWeight: '500',
+        textAlign: 'center',
+        fontFamily: 'Inter',
+        marginTop: 20
     },
     emptyStokNotif:{
-        justifyContent:'center',
-        alignItems:'center',
-        marginVertical:'50%', 
-        paddingHorizontal: 30
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: '50%', 
+        paddingHorizontal: 20,
+        width: '70%',
+    },
+    img: {
+        height: 113,
+        width: 82,
     }
 
 })
