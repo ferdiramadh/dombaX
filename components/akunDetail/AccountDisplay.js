6dad0154b2089c3ded10e8ca46e8c3e7aa734fac
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-06-05 20:43:23 +0700

    GEMBUL-45 Remove CustomHeader.

--- components/akunDetail/AccountDisplay.js
+++ components/akunDetail/AccountDisplay.js
@@ -7,7 +7,6 @@ import { pickImage } from '../../utils/ImageUpload';
 import { windowWidth, windowHeigth } from '../../utils/DimensionSetup'
 import EditAccount from './EditAccount';
 import DisplayProfile from './DisplayProfile';
-import CustomHeader from '../CustomHeader';
 
 const AccountDisplay = ({navigation}) => {
 
@@ -22,7 +21,6 @@ const AccountDisplay = ({navigation}) => {
   return (
       <ScrollView>
       <View style={styles.main}>
-      <CustomHeader leftSubMenu='Profil' styleFont={{fontSize:48}}/>
         <View style={styles.upperContent}>
         {isLoading?<ActivityIndicator size="small" color="orange" />: <View>
           {profileData.image ? <Image source={{ uri: profileData.image }} style={styles.photoProfileIcon} />:<MaterialIcons name="account-circle" size={100} color="black" />}</View>
@@ -50,63 +48,60 @@ export default AccountDisplay;
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    width:'100%',
+    width: '100%',
     backgroundColor: '#FFF'
   },
   lowerContent:{
-    width:'100%',  
-    justifyContent:'center',
-    alignItems:'center',  
+    width: '100%',  
+    justifyContent: 'center',
+    alignItems: 'center',  
     backgroundColor: '#FFF',
-    marginTop: 80
+    marginTop: 10
   },
   upperContent:{
-    width:'100%',
-    height:windowHeigth*.2,
-    justifyContent:'center',
-    alignItems:'center',
-    position: 'relative'
+    width: '100%',
+    height: windowHeigth*.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
   },
   textName:{
     fontSize: 28,
-    fontWeight:'500'
+    fontWeight: '500'
   },
   textCoorporateName:{
     fontSize: 30,
-    fontWeight:'bold'
+    fontWeight: 'bold'
   },
   text:{
     fontSize: 20,
-   
   },
   photoProfileIcon:{
-      width:100,
-      height:100,
-      borderRadius:60,
-      marginTop: 160
+      width: 100,
+      height: 100,
+      borderRadius: 60,
   },
   btnEdit:{
       height: 40,
       width: '15%',
-      backgroundColor:'white',
-      justifyContent:'center',
-      alignItems:'center',
-      borderRadius:5,
-      borderWidth:2,
-      marginTop:5
-      
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 5,
+      borderWidth: 2,
+      marginTop: 5
   },
   photoOptionsWrap: {
-    flexDirection:'row',
-    justifyContent:'space-around',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     marginVertical: 10,
     position: 'absolute',
-    bottom: -20,
+    bottom: 80,
     right: windowWidth/2 - 60
   },
   photoButton:{
-    justifyContent:'center',
-    alignItems:'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   progressBar: {
     width: 200,
@@ -116,7 +111,6 @@ const styles = StyleSheet.create({
   },
   progressPercentage:{
     height: '100%',
-    // width: '50%',
     backgroundColor: 'yellow'
   }
 });
