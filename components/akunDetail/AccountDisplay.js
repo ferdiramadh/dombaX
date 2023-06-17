708f7376757c4fe9bbc87d237f61833586adb0f5
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-06-17 23:55:40 +0700

    GEMBUL-49 Change parent container with View instead of ScrollView

--- components/akunDetail/AccountDisplay.js
+++ components/akunDetail/AccountDisplay.js
@@ -20,28 +20,25 @@ const AccountDisplay = ({navigation}) => {
   const [ percent, setPercent ] = useState("0%")
 
   return (
-      <ScrollView>
-      <View style={styles.main}>
-      <CustomHeader leftSubMenu='Profil' styleFont={{fontSize:48}}/>
+      <View style={{ flex: 1 }}>
+        <View style={styles.main}>
+        <CustomHeader leftSubMenu='Profil' styleFont={{fontSize:48}}/>
         <View style={styles.upperContent}>
-        {isLoading?<ActivityIndicator size="small" color="orange" />: <View>
-          {profileData.image ? <Image source={{ uri: profileData.image }} style={styles.photoProfileIcon} />:<MaterialIcons name="account-circle" size={100} color="black" />}</View>
-        }
-          <View style={styles.photoOptionsWrap}>
-             {isLoading?null:
-            <TouchableOpacity  onPress={() => pickImage("Profile", profileData.id, setIsLoading, "profile", setPercent, "image")} style={styles.photoButton}>
-              <FontAwesome name="camera" size={24} color="#ED9B83"/>
-            </TouchableOpacity>}
+            {isLoading?<ActivityIndicator size="small" color="orange" />: <View>
+              {profileData.image ? <Image source={{ uri: profileData.image }} style={styles.photoProfileIcon} />:<MaterialIcons name="account-circle" size={100} color="black" />}</View>
+            }
+            <View style={styles.photoOptionsWrap}>
+              {isLoading? null :
+              <TouchableOpacity  onPress={() => pickImage("Profile", profileData.id, setIsLoading, "profile", setPercent, "image")} style={styles.photoButton}>
+                <FontAwesome name="camera" size={24} color="#ED9B83"/>
+              </TouchableOpacity>}
+            </View>
           </View>
-
-          
-        </View>
-        
           <View style={styles.lowerContent}>
-            { isEdit? <EditAccount navigation={navigation} profileData={profileData} isEdit={isEdit} setIsEdit={setIsEdit}/>: <DisplayProfile profileData={profileData} isEdit={isEdit} setIsEdit={setIsEdit}/>}
+            { isEdit? <EditAccount navigation={navigation} profileData={profileData} isEdit={isEdit} setIsEdit={setIsEdit}/> : <DisplayProfile profileData={profileData} isEdit={isEdit} setIsEdit={setIsEdit}/>}
           </View>
+        </View>
       </View>
-      </ScrollView>
   );
 };
 
@@ -50,26 +47,25 @@ export default AccountDisplay;
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    width:'100%',
+    width: '100%',
     backgroundColor: '#FFF'
   },
   lowerContent:{
-    width:'100%',  
-    justifyContent:'center',
-    alignItems:'center',  
-    backgroundColor: '#FFF',
-    marginTop: 80
+    width: '100%',  
+    justifyContent: 'center',
+    alignItems: 'center',  
+    marginTop: 80,
   },
   upperContent:{
-    width:'100%',
-    height:windowHeigth*.2,
-    justifyContent:'center',
-    alignItems:'center',
+    width: '100%',
+    height: windowHeigth*.2,
+    justifyContent: 'center',
+    alignItems: 'center',
     position: 'relative'
   },
   textName:{
     fontSize: 28,
-    fontWeight:'500'
+    fontWeight: '500'
   },
   textCoorporateName:{
     fontSize: 30,
@@ -77,36 +73,34 @@ const styles = StyleSheet.create({
   },
   text:{
     fontSize: 20,
-   
   },
   photoProfileIcon:{
-      width:100,
-      height:100,
-      borderRadius:60,
+      width: 100,
+      height: 100,
+      borderRadius: 60,
       marginTop: 160
   },
   btnEdit:{
       height: 40,
       width: '15%',
-      backgroundColor:'white',
-      justifyContent:'center',
-      alignItems:'center',
-      borderRadius:5,
-      borderWidth:2,
-      marginTop:5
-      
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 5,
+      borderWidth: 2,
+      marginTop: 5
   },
   photoOptionsWrap: {
-    flexDirection:'row',
-    justifyContent:'space-around',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     marginVertical: 10,
     position: 'absolute',
     bottom: -20,
     right: windowWidth/2 - 60
   },
   photoButton:{
-    justifyContent:'center',
-    alignItems:'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   progressBar: {
     width: 200,
