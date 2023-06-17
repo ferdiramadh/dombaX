7dc4e694954436d5e817cf9b9161df6a2264c6ef
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-06-17 23:47:45 +0700

    GEMBUL-49 Simplify AccountScreen

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,21 +1,10 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import React from 'react';
-import AccountDisplay from '../components/akunDetail/AccountDisplay';
+import React from 'react'
+import AccountDisplay from '../components/akunDetail/AccountDisplay'
 
 const AccountScreen = ({navigation}) => {
- 
-  return (
-    <View style={styles.container}>
-       <AccountDisplay navigation={navigation}/>
-     </View>
-  );
-};
 
-export default AccountScreen;
+  return  <AccountDisplay navigation={navigation}/>
 
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor:'#FFF',  
-    }
-});
+}
+
+export default AccountScreen
