2c12280ccbd427bb7980a1e7659febf071ea8514
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-06-24 20:19:22 +0700

    GEMBUL-42 Format document, clean up, and refactoring.

--- components/stok/pakan/PakanForm.js
+++ components/stok/pakan/PakanForm.js
@@ -1,172 +1,166 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, TextInput, View,TouchableOpacity, Text, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, TextInput, View, TouchableOpacity, Text, Image } from 'react-native'
 import { MaterialIcons, AntDesign } from '@expo/vector-icons'
-import { pickImageOnly } from '../../../utils/ImageUpload';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { pickImageOnly } from '../../../utils/ImageUpload'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
-const PakanForm = ({setFieldValue,handleChange,handleBlur, values,handleSubmit,setModalVisible, modalVisible}) => {
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
+const PakanForm = ({ setFieldValue, handleChange, handleBlur, values, handleSubmit, setModalVisible, modalVisible }) => {
+  const [mode, setMode] = useState('date')
+  const [show, setShow] = useState(false)
 
   const onChange = (event, selectedDate) => {
-    if(selectedDate){
-        const currentDate = selectedDate;
-        setShow(false);
-        setFieldValue('kadaluarsa', selectedDate.toDateString())
+    if (selectedDate) {
+      setShow(false)
+      setFieldValue('kadaluarsa', selectedDate.toDateString())
     } else {
-        console.log("eweuh")
-        setShow(false);
-        setFieldValue('kadaluarsa', '')
+      setShow(false)
+      setFieldValue('kadaluarsa', '')
     }
-    
-  };
+
+  }
 
   const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
+    setShow(true)
+    setMode(currentMode)
+  }
 
   const showDatepicker = () => {
-    showMode('date');
-  };
+    showMode('date')
+  }
 
 
-  const [ img, setImg ] = useState()
+  const [img, setImg] = useState()
 
   const removePhoto = () => {
     setImg()
-    setFieldValue('image','')
+    setFieldValue('image', '')
   }
 
   useEffect(() => {
     setFieldValue('image', img)
-  },[img])
-    
-    return (
-          <View style={{width:'100%',flex: 1, justifyContent:'center',alignItems:'center', marginBottom:10}}>
-            {show && (
-              <DateTimePicker
-                testID="dateTimePicker"
-                value={new Date}
-                mode={mode}
-                is24Hour={true}
-                onChange={onChange}
-              />
-            )}
-            <TextInput
-              onChangeText={handleChange('nama')}
-              onBlur={handleBlur('nama')}
-              value={values.nama}
-              style={styles.textInput}
-              placeholder='Jenis Pakan'
-            />
-            <View style={{width:'90%',flex: 1}}>
-              <Text style={{color:'#474747'}}>Contoh: Silase</Text>
-            </View>
-            <View style={{width:'100%',height:'100%', backgroundColor:'transparent', flex: 1, justifyContent:'center',alignItems:'center'}}>
-            <TextInput
-              onChangeText={handleChange('merk')}
-              onBlur={handleBlur('merk')}
-              value={values.merk}
-              style={styles.textInput}
-              placeholder='Produsen'
-            />
-            <View style={{width:'90%',flex: 1}}>
-              <Text style={{color:'#474747'}}>Contoh: Anugrah Jaya Perkasa</Text>
-            </View>
-             <TextInput
-              onChangeText={handleChange('hargaBeli')}
-              onBlur={handleBlur('hargaBeli')}
-              value={values.hargaBeli}
-              style={styles.textInput}
-              placeholder='Harga Beli'
-              keyboardType='numeric'
-            />
-            <TextInput
-              onChangeText={handleChange('jumlah')}
-              onBlur={handleBlur('jumlah')}
-              value={values.jumlah}
-              style={styles.textInput}
-              placeholder='Jumlah (kg)'
-              keyboardType='numeric'
-            />
-            <TouchableOpacity style={styles.textInput} onPress={showDatepicker}>
-                <View style={{flexDirection:'row', justifyContent:'space-between', paddingRight:10}}>
-                    <Text style={{color:'#474747'}}>{values.kadaluarsa?values.kadaluarsa:"Kadaluarsa"}</Text>   
-                    <MaterialIcons name="date-range" size={24} color="black" />    
-                </View>                
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.textInput} onPress={() => {
-              let isUpdate = false
-              pickImageOnly(isUpdate, setImg)
-            }}>
-                <View style={{flexDirection:'row', justifyContent:'space-between', paddingRight:10}}>
-                    <Text style={{color:'#474747'}}>Upload Gambar</Text>   
-                    <MaterialIcons name="file-upload" size={24} color="black" />      
-                </View>                
-            </TouchableOpacity>
-            {img? <View style={{width: '90%', height:150,  justifyContent:'center', alignItems:'center', marginVertical: 30}}>
-                      <Image source={{ uri:img }} style={styles.imageWrap} />
-                      <TouchableOpacity style={{justifyContent:'center', alignItems:'center'}} onPress={removePhoto}>
-                        <AntDesign name="delete" size={24} color="lightgrey" /><Text style={{color:"grey"}}>Hapus Foto</Text>
-                      </TouchableOpacity>
-                    </View>:null}
-            <View style={styles.btnWrap}>
-              <TouchableOpacity style={styles.btnSave} onPress={() => setModalVisible(!modalVisible)}>
-                  <Text style={{fontSize:18, fontWeight:'700', textAlign:'center'}}>Batal</Text>                  
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.btnSave,{backgroundColor:'#ED9B83'}]} onPress={() => {
-                 setFieldValue("tipe", "pakan")
-                 handleSubmit()
-              }}>
-                  <Text style={{fontSize:18, fontWeight:'700', textAlign:'center',color:'#FFF'}}>Simpan</Text>                  
-              </TouchableOpacity>
-            </View>
-            </View>
+  }, [img])
+
+  return (
+    <View style={styles.container}>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={new Date}
+          mode={mode}
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+      <TextInput
+        onChangeText={handleChange('nama')}
+        onBlur={handleBlur('nama')}
+        value={values.nama}
+        style={styles.textInput}
+        placeholder='Jenis Pakan'
+      />
+      <View style={{ width: '90%', flex: 1 }}>
+        <Text style={{ color: '#474747' }}>Contoh: Silase</Text>
+      </View>
+      <View style={{ width: '100%', height: '100%', backgroundColor: 'transparent', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TextInput
+          onChangeText={handleChange('merk')}
+          onBlur={handleBlur('merk')}
+          value={values.merk}
+          style={styles.textInput}
+          placeholder='Produsen'
+        />
+        <View style={{ width: '90%', flex: 1 }}>
+          <Text style={{ color: '#474747' }}>Contoh: Anugrah Jaya Perkasa</Text>
+        </View>
+        <TextInput
+          onChangeText={handleChange('hargaBeli')}
+          onBlur={handleBlur('hargaBeli')}
+          value={values.hargaBeli}
+          style={styles.textInput}
+          placeholder='Harga Beli'
+          keyboardType='numeric'
+        />
+        <TextInput
+          onChangeText={handleChange('jumlah')}
+          onBlur={handleBlur('jumlah')}
+          value={values.jumlah}
+          style={styles.textInput}
+          placeholder='Jumlah (kg)'
+          keyboardType='numeric'
+        />
+        <TouchableOpacity style={styles.textInput} onPress={showDatepicker}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 10 }}>
+            <Text style={{ color: '#474747' }}>{values.kadaluarsa ? values.kadaluarsa : "Kadaluarsa"}</Text>
+            <MaterialIcons name="date-range" size={24} color="black" />
           </View>
-    )
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.textInput} onPress={() => {
+          let isUpdate = false
+          pickImageOnly(isUpdate, setImg)
+        }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 10 }}>
+            <Text style={{ color: '#474747' }}>Upload Gambar</Text>
+            <MaterialIcons name="file-upload" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
+        {img ? <View style={{ width: '90%', height: 150, justifyContent: 'center', alignItems: 'center', marginVertical: 30 }}>
+          <Image source={{ uri: img }} style={styles.imageWrap} />
+          <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center' }} onPress={removePhoto}>
+            <AntDesign name="delete" size={24} color="lightgrey" /><Text style={{ color: "grey" }}>Hapus Foto</Text>
+          </TouchableOpacity>
+        </View> : null}
+        <View style={styles.btnWrap}>
+          <TouchableOpacity style={styles.btnSave} onPress={() => setModalVisible(!modalVisible)}>
+            <Text style={{ fontSize: 18, fontWeight: '700', textAlign: 'center' }}>Batal</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btnSave, { backgroundColor: '#ED9B83' }]} onPress={() => {
+            setFieldValue("tipe", "pakan")
+            handleSubmit()
+          }}>
+            <Text style={{ fontSize: 18, fontWeight: '700', textAlign: 'center', color: '#FFF' }}>Simpan</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default PakanForm
 
 const styles = StyleSheet.create({
-    container:{
-    width:'100%',
-    flexDirection:'column',
-    marginBottom:15,
-    borderBottomColor:'lightgrey',
-    borderBottomWidth: 2
-},
-    textInput:{
-      backgroundColor:'#DFE1E0',
-      width:'90%',
-      height:50,                       
-      // borderColor:'black',
-      // borderWidth:2,                
-      // borderRadius:20,
-      justifyContent:'center', 
-      paddingLeft:20,
-      marginVertical:10,
-      borderRadius: 5
-    },
-    btnSave:{
-      backgroundColor:'white',
-      width:'45%',
-      height:40,                       
-      justifyContent:'center',
-      borderRadius:5,
-      elevation: 2
-    },
-    btnWrap:{
-      flexDirection:'row',
-      justifyContent:'space-around',
-      width:'100%',
-      paddingHorizontal: 10
-    },
-    imageWrap:{
-      width: undefined,
-      height: '100%',
-      aspectRatio: 1
-      
-    }
+  container: { 
+    width: '100%', 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    marginBottom: 10 
+  },
+  textInput: {
+    backgroundColor: '#DFE1E0',
+    width: '90%',
+    height: 50,
+    justifyContent: 'center',
+    paddingLeft: 20,
+    marginVertical: 10,
+    borderRadius: 5
+  },
+  btnSave: {
+    backgroundColor: 'white',
+    width: '45%',
+    height: 40,
+    justifyContent: 'center',
+    borderRadius: 5,
+    elevation: 2
+  },
+  btnWrap: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    paddingHorizontal: 10
+  },
+  imageWrap: {
+    width: undefined,
+    height: '100%',
+    aspectRatio: 1
+  }
 })
