fdf44b065eb998faa810a35987384f04495759ef
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-07-02 14:03:07 +0700

    GEMBUL-40 Implement DisplayedDateWithName on DatePickerField, clean up, and formatting document.

--- components/selectedproduct/DatePickerField.js
+++ components/selectedproduct/DatePickerField.js
@@ -1,31 +1,36 @@
-import { View, Text,TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { useEffect } from 'react'
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'
+import { DisplayedDateWithName } from '../../utils/DisplayDate'
 
-const DatePickerField = ({showDatepicker, setFieldValue, date, values}) => {
+const DatePickerField = ({ showDatepicker, setFieldValue, date, values }) => {
 
   useEffect(() => {
-      if(date) {
-        setFieldValue("kadaluarsa", date)
-      } 
-      
-  },[date])
-  return (
-    <TouchableOpacity style={{
-        backgroundColor:'#DFE1E0',
-        width:'100%',
-        height:50,                       
+    if (date) {
+      setFieldValue("kadaluarsa", date)
+    }
 
-        justifyContent:'center', 
-        paddingLeft:20,
-        marginVertical:10,
-      }} onPress={showDatepicker}>
-        <View style={{flexDirection:'row', justifyContent:'space-between', paddingRight:10}}>
-            <Text style={{color:'#474747'}}>{values.kadaluarsa}</Text>   
-            <MaterialIcons name="date-range" size={24} color="black" />    
-        </View>                
+  }, [date])
+  
+  return (
+    <TouchableOpacity style={styles.wrapper} onPress={showDatepicker}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 10 }}>
+        <Text style={{ color: '#474747' }}>{DisplayedDateWithName(values.kadaluarsa)}</Text>
+        <MaterialIcons name="date-range" size={24} color="black" />
+      </View>
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#DFE1E0',
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    paddingLeft: 20,
+    marginVertical: 10,
+  }
+})
 
 export default DatePickerField
