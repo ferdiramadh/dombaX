b393619ede33228fe2316242e57c51bee51b7d9e
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-07-02 13:56:31 +0700

    GEMBUL-40 Create DisplayedDateWithName function.

--- utils/DisplayDate.js
+++ utils/DisplayDate.js
@@ -3,9 +3,7 @@ const hari = [ "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu" ]
 const separator = "-"
 
 const checkMoreThanTen = (val) => {
-    if(val < 10) 
-    return "0" + val
-
+    if(val < 10) return "0" + val
     return val
 }
 
@@ -17,4 +15,15 @@ export const DisplayedDate = (val) => {
     const displayDate = day + separator + month + separator + year
 
     return displayDate
+}
+
+export const DisplayedDateWithName = (val) => {
+    const date = new Date(val)
+    const dayName = hari[date.getDay()]
+    const day = checkMoreThanTen(date.getDate())
+    const month = checkMoreThanTen(date.getMonth() + 1)
+    const year = date.getFullYear()
+    const displayDateWithName = dayName + " " + day + separator + month + separator + year
+
+    return displayDateWithName
 }
