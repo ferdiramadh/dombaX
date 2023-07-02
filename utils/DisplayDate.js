563cd64c9d33b4fa83029735c0842a3f83af140e
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-07-02 13:43:29 +0700

    GEMBUL-40 Develop display date utils.

---
+++ utils/DisplayDate.js
@@ -0,0 +1,20 @@
+const hari = [ "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu" ]
+
+const separator = "-"
+
+const checkMoreThanTen = (val) => {
+    if(val < 10) 
+    return "0" + val
+
+    return val
+}
+
+export const DisplayedDate = (val) => {
+    const date = new Date(val)
+    const day = checkMoreThanTen(date.getDate())
+    const month = checkMoreThanTen(date.getMonth() + 1)
+    const year = date.getFullYear()
+    const displayDate = day + separator + month + separator + year
+
+    return displayDate
+}
