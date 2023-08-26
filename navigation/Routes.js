7a72c99ac789bd2bab916aa7a96501903c04cb41
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-08-26 20:23:49 +0700

    GEMBUL-57 Create CustomFonts and implement it to Routes.

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -4,13 +4,8 @@ import AuthStack from './AuthStack'
 import { ActivityIndicator, LogBox, StyleSheet, View } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as SplashScreen from 'expo-splash-screen'
-import { useFonts, Inter_400Regular, Inter_300Light, Inter_600SemiBold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
-import {
-    Baloo2_500Medium,
-    Baloo2_600SemiBold,
-    Baloo2_700Bold,
-    Baloo2_800ExtraBold,
-} from '@expo-google-fonts/baloo-2'
+import { useFonts } from '@expo-google-fonts/inter'
+import { CustomFonts } from '../utils/CustomFonts'
 
 SplashScreen.preventAutoHideAsync()
 
@@ -20,26 +15,7 @@ const Routes = () => {
 
     const [isLoading, setIsLoading] = useState(true)
     const [isSignIn, setIsSignIn] = useState(false)
-    let customFonts = {
-        'Baloo': require('../assets/font/baloo/Baloo.ttf'),
-        'Baloo-Medium' : Baloo2_500Medium,
-        'Baloo-SemiBold' : Baloo2_600SemiBold,
-        'Baloo-Bold' : Baloo2_700Bold,
-        'Baloo-ExtraBold' : Baloo2_800ExtraBold,
-        'Inter-Light': Inter_300Light,
-        'Inter': Inter_400Regular,
-        'Inter-SemiBold': Inter_600SemiBold,
-        'Inter-Bold': Inter_800ExtraBold,
-        'Poppins': require('../assets/font/poppins/Poppins-Medium.ttf'),
-        'PoppinsBold': require('../assets/font/poppins/Poppins-Bold.ttf'),
-        'QuickSand': require('../assets/font/quicksand/Quicksand-Regular.ttf'),
-        'QuickSand-Light': require('../assets/font/quicksand/Quicksand-Light.ttf'),
-        'QuickSand-Med': require('../assets/font/quicksand/Quicksand-Medium.ttf'),
-        'QuickSand-SemiBold': require('../assets/font/quicksand/Quicksand-SemiBold.ttf'),
-        'QuickSand-Bold': require('../assets/font/quicksand/Quicksand-Bold.ttf'),
-        
-    }
-    const [fontsLoaded] = useFonts(customFonts)
+    const [fontsLoaded] = useFonts(CustomFonts)
     const getData = async () => {
         try {
             const value = await AsyncStorage.getItem('@storage_Key')
---
+++ utils/CustomFonts.js
@@ -0,0 +1,21 @@
+import { Inter_400Regular, Inter_300Light, Inter_600SemiBold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { Baloo2_500Medium, Baloo2_600SemiBold, Baloo2_700Bold, Baloo2_800ExtraBold } from '@expo-google-fonts/baloo-2'
+
+export const CustomFonts = {
+    'Baloo': require('../assets/font/baloo/Baloo.ttf'),
+    'Baloo-Medium': Baloo2_500Medium,
+    'Baloo-SemiBold': Baloo2_600SemiBold,
+    'Baloo-Bold': Baloo2_700Bold,
+    'Baloo-ExtraBold': Baloo2_800ExtraBold,
+    'Inter-Light': Inter_300Light,
+    'Inter': Inter_400Regular,
+    'Inter-SemiBold': Inter_600SemiBold,
+    'Inter-Bold': Inter_800ExtraBold,
+    'Poppins': require('../assets/font/poppins/Poppins-Medium.ttf'),
+    'PoppinsBold': require('../assets/font/poppins/Poppins-Bold.ttf'),
+    'QuickSand': require('../assets/font/quicksand/Quicksand-Regular.ttf'),
+    'QuickSand-Light': require('../assets/font/quicksand/Quicksand-Light.ttf'),
+    'QuickSand-Med': require('../assets/font/quicksand/Quicksand-Medium.ttf'),
+    'QuickSand-SemiBold': require('../assets/font/quicksand/Quicksand-SemiBold.ttf'),
+    'QuickSand-Bold': require('../assets/font/quicksand/Quicksand-Bold.ttf'),
+}
