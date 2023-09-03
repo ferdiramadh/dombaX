65e1efc4a36ba496b059b60f31074ebd2403459e
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-09-03 17:01:09 +0700

    GEMBUL-61 Update UI on Income Transaction Components.

--- components/income/IncomeItem.js
+++ components/income/IncomeItem.js
@@ -6,90 +6,93 @@ import { FontAwesome } from '@expo/vector-icons'
 import { DeleteOptionContext } from '../../context/DeleteOptionContext'
 import { DisplayedDate } from '../../utils/DisplayDate'
 
-const IncomeItem = ({item, editItem}) => {
+const IncomeItem = ({ item, editItem }) => {
 
-    const purchaseCategoryIcon = 
-        {
-            penjualan: require('../../assets/images/purchasingcategory/Sell.png'),
-            modal: require('../../assets/images/purchasingcategory/Request_Money.png'),
-            hibah: require('../../assets/images/purchasingcategory/Gift.png'),
-            pinjam: require('../../assets/images/purchasingcategory/Lend.png'),
-            piutang: require('../../assets/images/purchasingcategory/Piutang.png'),
-            
-        }
+    const purchaseCategoryIcon =
+    {
+        penjualan: require('../../assets/images/purchasingcategory/Sell.png'),
+        modal: require('../../assets/images/purchasingcategory/Request_Money.png'),
+        hibah: require('../../assets/images/purchasingcategory/Gift.png'),
+        pinjam: require('../../assets/images/purchasingcategory/Lend.png'),
+        piutang: require('../../assets/images/purchasingcategory/Piutang.png'),
+
+    }
 
     const { deleteOpt, CheckIfInList, AddOrRemoveList, setDeleteOpt } = useContext(DeleteOptionContext)
 
-    
-  return (
-    <TouchableOpacity 
-        key={item.id} 
-        style={styles.container} 
-        onPress={() => editItem(item)}
-        onLongPress={() => setDeleteOpt(prev => ({...prev, selectDelete: !deleteOpt.selectDelete}))}
-    >
-        <View style={styles.iconWrapper}>
-            {item.kategori == 'Penjualan'?<Image source={purchaseCategoryIcon.penjualan} style={styles.img} resizeMode='contain'/>: null  }
-            {item.kategori == 'Penambahan Modal'?<Image source={purchaseCategoryIcon.modal} style={styles.img} resizeMode='contain'/>: null  }
-            {item.kategori == 'Hibah'?<Image source={purchaseCategoryIcon.hibah} style={styles.img} resizeMode='contain'/>: null  }
-            {item.kategori == 'Pinjaman'?<Image source={purchaseCategoryIcon.pinjam} style={styles.img} resizeMode='contain'/>: null  }     
-            {item.kategori == 'Piutang'?<Image source={purchaseCategoryIcon.piutang} style={styles.img} resizeMode='contain'/>: null  }     
-        </View>
-        <View style={styles.mainWrapper}>
-        <Text style={{ fontSize: 18, fontFamily: 'Inter', fontWeight: '600'}} numberOfLines={2} ellipsizeMode='tail'>{item.namaTransaksi}</Text>
-            <Text style={{color: '#B3B3B3'}}>{DisplayedDate(item.tanggal)}</Text>
-        </View>
-        <View style={styles.rightWrapper}>
-            <View style={styles.upperRight}>
-                <Text style={{ fontSize: 16, fontFamily: 'Inter', fontWeight: 'bold'}}>{formatToCurrencyLight(item.jumlah)}</Text>
-                {
-                !deleteOpt.allDelete? null :
-                <View style={styles.buttonSection}>
-                    <TouchableOpacity onPress={() => AddOrRemoveList(item)}>
-                            {CheckIfInList(item)?<FontAwesome name="check-square" size={19} color="#ED9B83" /> : <View style={styles.square} />}
-                    </TouchableOpacity>
-                </View>
-                }
-                { 
-                deleteOpt.selectDelete && !deleteOpt.allDelete? 
-                <View style={styles.buttonSection}>
-                    <TouchableOpacity onPress={() => AddOrRemoveList(item)}>
-                            {CheckIfInList(item)?<FontAwesome name="check-square" size={19} color="#ED9B83" /> : <View style={styles.square} />}
-                    </TouchableOpacity> 
-                </View> : null
-                }
+
+    return (
+        <TouchableOpacity
+            key={item.id}
+            style={styles.container}
+            onPress={() => editItem(item)}
+            onLongPress={() => setDeleteOpt(prev => ({ ...prev, selectDelete: !deleteOpt.selectDelete }))}
+        >
+            <View style={styles.iconWrapper}>
+                {item.kategori == 'Penjualan' ? <Image source={purchaseCategoryIcon.penjualan} style={styles.img} resizeMode='contain' /> : null}
+                {item.kategori == 'Penambahan Modal' ? <Image source={purchaseCategoryIcon.modal} style={styles.img} resizeMode='contain' /> : null}
+                {item.kategori == 'Hibah' ? <Image source={purchaseCategoryIcon.hibah} style={styles.img} resizeMode='contain' /> : null}
+                {item.kategori == 'Pinjaman' ? <Image source={purchaseCategoryIcon.pinjam} style={styles.img} resizeMode='contain' /> : null}
+                {item.kategori == 'Piutang' ? <Image source={purchaseCategoryIcon.piutang} style={styles.img} resizeMode='contain' /> : null}
             </View>
-            <Text style={{color: '#000'}} ellipsizeMode='tail' numberOfLines={1}>{item.kategori}</Text>
-            {(item.kategori == 'Penjualan' || item.kategori == 'Pinjaman' || item.kategori == 'Piutang') && item.statusBayar !== 'status'?
-            <View style={[styles.status, item.statusBayar == 'Lunas'?{backgroundColor:'#43B88E'}:{backgroundColor:'#EB3223'}]}>
-                <Text style={styles.statusText}>{item.statusBayar == 'Lunas'?'Lunas': 'Belum Lunas'}</Text>
-            </View>: null}
-        </View>
-    </TouchableOpacity>
-  )
+            <View style={styles.mainWrapper}>
+                <Text style={styles.namaTransaksiTxt} numberOfLines={2} ellipsizeMode='tail'>{item.namaTransaksi}</Text>
+                <Text style={styles.dateTxt}>{DisplayedDate(item.tanggal)}</Text>
+            </View>
+            <View style={styles.rightWrapper}>
+                <View style={styles.upperRight}>
+                    <Text style={styles.jumlahTxt}>{formatToCurrencyLight(item.jumlah)}</Text>
+                    {
+                        !deleteOpt.allDelete ? null :
+                            <View style={styles.buttonSection}>
+                                <TouchableOpacity onPress={() => AddOrRemoveList(item)}>
+                                    {CheckIfInList(item) ? <FontAwesome name="check-square" size={19} color="#ED9B83" /> : <View style={styles.square} />}
+                                </TouchableOpacity>
+                            </View>
+                    }
+                    {
+                        deleteOpt.selectDelete && !deleteOpt.allDelete ?
+                            <View style={styles.buttonSection}>
+                                <TouchableOpacity onPress={() => AddOrRemoveList(item)}>
+                                    {CheckIfInList(item) ? <FontAwesome name="check-square" size={19} color="#ED9B83" /> : <View style={styles.square} />}
+                                </TouchableOpacity>
+                            </View> : null
+                    }
+                </View>
+                <Text style={styles.kategoriTxt} ellipsizeMode='tail' numberOfLines={1}>{item.kategori}</Text>
+                {(item.kategori == 'Penjualan' || item.kategori == 'Pinjaman' || item.kategori == 'Piutang') && item.statusBayar !== 'status' ?
+                    <View style={[styles.status, item.statusBayar == 'Lunas' ? { backgroundColor: '#43B88E' } : { backgroundColor: '#EB3223' }]}>
+                        <Text style={styles.statusText}>{item.statusBayar == 'Lunas' ? 'Lunas' : 'Belum Lunas'}</Text>
+                    </View> : null}
+            </View>
+        </TouchableOpacity>
+    )
 }
 
 export default IncomeItem
 
 const styles = StyleSheet.create({
-    container:{
-        width: windowWidth, 
-        height: windowHeigth*.1, 
-        marginBottom: 5,
+    container: {
+        width: windowWidth,
+        height: windowHeigth * .12,
         flexDirection: 'row',
-        paddingHorizontal: 5
+        padding: 10,
+        backgroundColor: '#F9F9F9'
     },
     iconWrapper: {
-        height: '100%',
-        width: '20%',
+        width: undefined,
+        height: '90%',
+        aspectRatio: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#DAEFDD',
+        borderRadius: 20
     },
     mainWrapper: {
         height: '100%',
         width: '40%',
         paddingTop: 5,
-        paddingLeft: 5  
+        paddingLeft: 10
     },
     rightWrapper: {
         height: '100%',
@@ -114,22 +117,39 @@ const styles = StyleSheet.create({
     },
     statusText: {
         color: '#FFF',
-        fontFamily: 'Inter',
-        fontWeight: 'bold'
-    }, 
+        fontFamily: 'Quicksand-SemiBold',
+    },
     upperRight: {
-        flexDirection:'row',  
+        flexDirection: 'row',
         paddingVertical: 5,
         alignItems: 'center'
     },
-    buttonSection:{
-        flexDirection:'row',  
+    buttonSection: {
+        flexDirection: 'row',
         padding: 5,
-        justifyContent:'flex-end',  
+        justifyContent: 'flex-end',
     },
     square: {
-        width: 18, 
-        height: 18, 
+        width: 18,
+        height: 18,
         borderWidth: 1
+    },
+    namaTransaksiTxt: { 
+        fontSize: 16, 
+        fontFamily: 'Quicksand' 
+    },
+    dateTxt: { 
+        color: '#B3B3B3', 
+        fontSize: 14, 
+        fontFamily: 'Quicksand' 
+    },
+    jumlahTxt: { 
+        fontSize: 16, 
+        fontFamily: 'Quicksand-Bold' 
+    },
+    kategoriTxt: { 
+        color: '#000', 
+        fontSize: 14, 
+        fontFamily: 'Quicksand' 
     }
 })
--- components/income/IncomeSection.js
+++ components/income/IncomeSection.js
@@ -4,114 +4,112 @@ import { windowHeigth, windowWidth } from '../../utils/DimensionSetup'
 import IncomeItem from './IncomeItem'
 import { formatTotalToCurrency } from '../../utils/FormatCurrency'
 import firebase from '../../Firebaseconfig'
-import { useNavigation } from '@react-navigation/native';
-import { FilterTransactionContext } from '../../context/FilterTransactionContext';
-import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+import { FilterTransactionContext } from '../../context/FilterTransactionContext'
+import { MaterialIcons } from '@expo/vector-icons'
 import { DeleteOptionContext } from '../../context/DeleteOptionContext'
 
-const IncomeSection = ({listIncome,searchItems, isSearch, searchKeyword, isLoading, setSearchItems}) => {
+const IncomeSection = ({ listIncome, searchItems, isSearch, searchKeyword, isLoading, setSearchItems }) => {
   const { isFilter, filteredList, filterBy, setIsFilter } = useContext(FilterTransactionContext)
   const { DeleteOptionSection, deleteOpt } = useContext(DeleteOptionContext)
-  const [editData, setEditData] = useState({});
-  const navigation = useNavigation();
+  const [editData, setEditData] = useState({})
+  const navigation = useNavigation()
 
   const sortData = listIncome.sort((a, b) => {
-    let bd = objToDate(b.createdAt);
-    let ad = objToDate(a.createdAt);
-    return ad - bd;
-  });
+    let bd = objToDate(b.createdAt)
+    let ad = objToDate(a.createdAt)
+    return ad - bd
+  })
 
-  function objToDate (obj) {
-    let result = new Date(0);
-    if( obj !== null) {
-        result.setSeconds(obj.seconds);
-        result.setMilliseconds(obj.nanoseconds/1000000);
-        return result;
+  function objToDate(obj) {
+    let result = new Date(0)
+    if (obj !== null) {
+      result.setSeconds(obj.seconds)
+      result.setMilliseconds(obj.nanoseconds / 1000000)
+      return result
     }
-    
+
   }
   function getSum(arr, jumlah) {
     return arr.reduce((total, obj) => {
       if (typeof obj[jumlah] === 'string') {
-        return total +  parseInt(obj.jumlah);;
+        return total + parseInt(obj.jumlah)
       }
-      return total +  parseInt(obj.jumlah);;
-    }, 0);
+      return total + parseInt(obj.jumlah)
+    }, 0)
   }
 
   const editItem = (item) => {
-        
-    return firebase
-    .firestore()
-    .collection("income")
-    .doc(item.id)
-    .get()
-    .then((i) => {
-        setEditData(i.data());
-    })
-    
-    
-}
 
-useEffect(() => {
-  console.log("Cek Data")
-  if(Object.keys(editData).length !== 0) {
-      // setGlobalModalVisible(!modalGlobalVisible)
-      console.log("ada nih edit data"+editData)
-      navigation.navigate("IncomeDetail",{editData, navigation, isSearch, searchItems, setSearchItems})
-  } if (editData !== undefined) {
-      console.log("MANA nih edit data")
-      // navigation.navigate("DetailProduct",{editData, setEditData})
+    return firebase
+      .firestore()
+      .collection("income")
+      .doc(item.id)
+      .get()
+      .then((i) => {
+        setEditData(i.data())
+      })
+
+
   }
 
-},[editData])
+  useEffect(() => {
+
+    if (Object.keys(editData).length !== 0) {
+      navigation.navigate("IncomeDetail", { editData, navigation, isSearch, searchItems, setSearchItems })
+    } if (editData == undefined) {
+      alert("Tidak ada data untuk diedit!")
+
+    }
+
+  }, [editData])
 
   return (
     <View style={styles.container}>
-      
-      {isLoading? <View style={styles.loaderContainer}>
-              <ActivityIndicator size="large" color="orange" />
-                </View>:
-      <ScrollView >
-        {!isSearch && !isFilter && <View style={styles.totalIncomeWrapper}>
-        <View>
-          <Text style={styles.totalIncomeTitle}>Total Pemasukan</Text>   
-          <Text style={styles.totalIncomeCount}>{formatTotalToCurrency(getSum(listIncome, "jumlah"))}</Text>
-        </View> 
-      </View> }
-            {
-            deleteOpt.selectDelete && <DeleteOptionSection dataProps={{ dataList: sortData, collection:'income', storageCollection: 'Income' }}/>
-            }
-            {isSearch? <View style={{paddingTop: 10}}> 
-                  
-                  <Text style={{marginLeft: 20, marginBottom: 15}}>{searchItems.length} hasil ditemukan untuk "{searchKeyword}"</Text>
-                {
-                  searchItems.map((item, i) => {
-                    return <IncomeItem item={item} key={item.id} editItem={editItem}/>
-                  }) 
-                }
-              </View>: null}
-              {isFilter? <View style={{paddingTop: 10}}> 
-                  
-              <View style={{width: '100%', flexDirection:'row', justifyContent: 'space-around', marginBottom: 10}}>
-                <Text>Filter Berdasarkan {filterBy[0]['sortBy']}</Text><TouchableOpacity style={{flexDirection:'row'}} onPress={() => setIsFilter(false)}>
-                    <Text> Ulang Penyaringan.</Text>
-                    <MaterialIcons name="refresh" size={20} color="black" />
-                </TouchableOpacity>
-              </View>
-                {
-                  filteredList.map((item, i) => {
-                    return <IncomeItem item={item} key={item.id} editItem={editItem}/>
-                  }) 
-                }
-              </View>: null}
 
-            {sortData.length > 0 && !isSearch && !isFilter? sortData.map((item, i) => {
-                return <IncomeItem item={item} key={item.id} editItem={editItem}/>
-                })
-             : null}
-             <View style={{width: windowWidth, height: 10}}></View>
-      </ScrollView>}
+      {isLoading ? <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="orange" />
+      </View> :
+        <ScrollView >
+          {!isSearch && !isFilter && <View style={styles.totalIncomeWrapper}>
+            <View>
+              <Text style={styles.totalIncomeTitle}>Total Pemasukan</Text>
+              <Text style={styles.totalIncomeCount}>{formatTotalToCurrency(getSum(listIncome, "jumlah"))}</Text>
+            </View>
+          </View>}
+          {
+            deleteOpt.selectDelete && <DeleteOptionSection dataProps={{ dataList: sortData, collection: 'income', storageCollection: 'Income' }} />
+          }
+          {isSearch ? <View style={{ paddingTop: 10 }}>
+
+            <Text style={{ marginLeft: 20, marginBottom: 15 }}>{searchItems.length} hasil ditemukan untuk "{searchKeyword}"</Text>
+            {
+              searchItems.map((item, i) => {
+                return <IncomeItem item={item} key={item.id} editItem={editItem} />
+              })
+            }
+          </View> : null}
+          {isFilter ? <View style={{ paddingTop: 10 }}>
+
+            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }}>
+              <Text style={{ fontFamily: 'Quicksand' }}>Filter Berdasarkan {filterBy[0]['sortBy']}</Text><TouchableOpacity style={{ flexDirection: 'row' }} onPress={() => setIsFilter(false)}>
+                <Text style={{ fontFamily: 'Quicksand' }}> Ulang Penyaringan.</Text>
+                <MaterialIcons name="refresh" size={20} color="black" />
+              </TouchableOpacity>
+            </View>
+            {
+              filteredList.map((item, i) => {
+                return <IncomeItem item={item} key={item.id} editItem={editItem} />
+              })
+            }
+          </View> : null}
+
+          {sortData.length > 0 && !isSearch && !isFilter ? sortData.map((item, i) => {
+            return <IncomeItem item={item} key={item.id} editItem={editItem} />
+          })
+            : null}
+          <View style={{ width: windowWidth, height: 10 }}></View>
+        </ScrollView>}
     </View>
   )
 }
@@ -119,35 +117,29 @@ useEffect(() => {
 export default IncomeSection
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,     
-        height: windowHeigth,
-        width:windowWidth,
-         
-        
-    },
-    totalIncomeWrapper: {
-        width: windowWidth,
-        height: windowHeigth*.1,
-        // backgroundColor:'green',
-        paddingVertical: 5,
-        paddingLeft: 20
-    },
-    totalIncomeTitle:{
-        fontSize: 22,
-        fontWeight:'700',
-        fontFamily:'Inter'
-    },
-    totalIncomeCount:{
-        fontSize: 26,
-        fontWeight:'700',
-        fontFamily:'Inter',
-        color: '#43B88E'
-    }, 
-    loaderContainer:{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-    //   backgroundColor: 'red',
-    }
+  container: {
+    flex: 1,
+    height: windowHeigth,
+    width: windowWidth,
+  },
+  totalIncomeWrapper: {
+    width: windowWidth,
+    height: windowHeigth * .1,
+    paddingVertical: 5,
+    paddingLeft: 20
+  },
+  totalIncomeTitle: {
+    fontSize: 22,
+    fontFamily: 'Quicksand-Bold'
+  },
+  totalIncomeCount: {
+    fontSize: 26,
+    fontFamily: 'Quicksand-Bold',
+    color: '#43B88E'
+  },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 })
