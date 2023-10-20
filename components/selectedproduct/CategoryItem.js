db137e836a8abdc9b02d6cabb99e7257d6d905d5
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-10-20 23:12:43 +0700

    GEMBUL-70 Update UI for Ubah and Hapus button.

--- components/selectedproduct/CategoryItem.js
+++ components/selectedproduct/CategoryItem.js
@@ -1,22 +1,46 @@
-import { StyleSheet, Text, TouchableOpacity,View } from 'react-native'
-import React, { useState, useEffect} from 'react'
-import { Feather } from '@expo/vector-icons';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { AntDesign, Entypo } from '@expo/vector-icons'
 
-const CategoryItem = ({item, deleteItem, editItem,editData, setCategoryValue}) => {
-    
-  return (
-    <TouchableOpacity style={styles.container} onPress={() => setCategoryValue(item.name)}> 
-      <Text style={styles.text}>{item.name}</Text>
-      <View style={styles.buttonSection}>
-            <TouchableOpacity style={{marginLeft:10}} onPress={() => deleteItem(item)}>
-                <Feather name="trash-2" size={24} color="black" />
+const CategoryItem = ({ item, deleteItem, editItem, setCategoryValue }) => {
+    const [isPressed, setIsPressed] = useState(false)
+    return (
+        <TouchableOpacity style={[styles.container, isPressed ? { height: 100, paddingTop: 10 } : { height: 50, alignItems: 'center', }]} onPress={() => setCategoryValue(item.name)} disabled={isPressed ? true : false}>
+            <View style={styles.itemName}>
+                <Text style={styles.text}>{item.name}</Text>
+            </View>
+            <View style={styles.buttonSection}>
+                <TouchableOpacity style={{ marginLeft: 10 }} onPress={() => setIsPressed(!isPressed)}>
+                    <Entypo name="dots-three-horizontal" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            {isPressed && <FloatingModal deleteItem={deleteItem} editItem={editItem} item={item} setIsPressed=
+                {setIsPressed} />}
+        </TouchableOpacity>
+    )
+}
+
+const FloatingModal = ({ editItem, deleteItem, item, setIsPressed }) => {
+    return (
+        <View style={styles.floatingModal}>
+            <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => {
+                    setIsPressed(false)
+                }}>
+                <AntDesign name="closecircle" size={20} color="#DFE1E0" />
             </TouchableOpacity>
-            <TouchableOpacity style={{marginLeft:10}} onPress={() => editItem(item)}>
-                <Feather name="edit" size={24} color="black" />
-            </TouchableOpacity>  
+            <TouchableOpacity style={styles.btn} onPress={() => {
+                editItem(item)
+                // setIsPressed(false)
+            }}>
+                <Text style={styles.btnText}>Ubah</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn} onPress={() => deleteItem(item)}>
+                <Text style={styles.btnText}>Hapus</Text>
+            </TouchableOpacity>
         </View>
-    </TouchableOpacity>
-  )
+    )
 }
 
 export default CategoryItem
@@ -24,23 +48,57 @@ export default CategoryItem
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height:50,
-       
         paddingHorizontal: 20,
-        justifyContent:'center',
+        justifyContent: 'center',
         borderBottomWidth: 1,
-        borderBottomColor:'#DFE1E0'
+        borderBottomColor: '#DFE1E0',
+        flexDirection: 'row'
+    },
+    itemName: {
+        flex: 1,
     },
     text: {
-        fontSize:18
+        fontSize: 18,
     },
-    buttonSection:{
-        // backgroundColor:'red',
-        flexDirection:'row',  
-        width:'50%',
-        justifyContent:'flex-end',
-        position:'absolute',
-        right: 10
-       
+    buttonSection: {
+        flexDirection: 'row',
+        width: '30%',
+        justifyContent: 'flex-end',
+        right: 10,
+        paddingRight: 10
+    },
+    floatingModal: {
+        backgroundColor: '#FFFDFD',
+        position: 'absolute',
+        height: 80,
+        width: 150,
+        bottom: 10,
+        borderRadius: 10,
+        right: 10,
+        borderRadius: 10,
+        borderColor: '#DFE1E0',
+        borderWidth: 1
+    },
+    btn: {
+        flex: 1,
+        backgroundColor: '#FFFDFD',
+        justifyContent: 'center',
+        padding: 10,
+        borderRadius: 10,
+    },
+    closeButton: {
+        borderRadius: 20,
+        padding: 8,
+        position: 'absolute',
+        bottom: 10,
+        width: 50,
+        height: 50,
+        top: -15,
+        right: -20,
+        zIndex: 10
+    },
+    btnText: {
+        fontFamily: 'Inter',
+        fontWeight: '700'
     }
 })
