770de6e35b34d32064b951b33af5fea2d87eb862
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-10-28 14:15:34 +0700

    GEMBUL-69 Update tambah foto button on photosectiononboarding.

--- components/akunDetail/PhotoSectionOnboarding.js
+++ components/akunDetail/PhotoSectionOnboarding.js
@@ -1,23 +1,21 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
-import { pickImageOnly } from '../../utils/ImageUpload';
-import { windowWidth, windowHeigth } from '../../utils/DimensionSetup'
+import { MaterialIcons } from '@expo/vector-icons'
+import { pickImageOnly } from '../../utils/ImageUpload'
+import { windowHeigth } from '../../utils/DimensionSetup'
 
-const PhotoSectionOnboarding = ({title, value, setFieldValue,field}) => {
+const PhotoSectionOnboarding = ({ title, value, setFieldValue, field }) => {
   return (
     <View style={styles.upperContent}>
-        <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title}>{title}</Text>
+      <View>
         <View>
-            <View>  
-            {value? <Image source={{ uri: value }} style={styles.photoProfileIcon} />: <MaterialIcons name="account-circle" size={100} color="black" /> }
-            </View>
+          {value ? <Image source={{ uri: value }} style={styles.photoProfileIcon} /> : <MaterialIcons name="account-circle" size={100} color="black" />}
         </View>
-        <View style={styles.photoOptionsWrap}>
-            <TouchableOpacity onPress={() => pickImageOnly(true, setFieldValue, field)} style={styles.photoButton}>
-                <FontAwesome name="camera" size={24} color="#ED9B83"/>
-            </TouchableOpacity>
-        </View>
+      </View>
+      <TouchableOpacity onPress={() => pickImageOnly(true, setFieldValue, field)} style={styles.tambahFotoBtn}>
+        <Text style={styles.tambahFotoBtnTxt}>Tambah Foto</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -25,35 +23,31 @@ const PhotoSectionOnboarding = ({title, value, setFieldValue,field}) => {
 export default PhotoSectionOnboarding
 
 const styles = StyleSheet.create({
-    upperContent:{
-        width:'100%',
-        height:windowHeigth*.2,
-        justifyContent:'center',
-        alignItems:'center',
-        position: 'relative',
-        padding: 20
-      },
-      title:{
-        alignSelf: 'flex-start',
-        fontSize: 24,
-        fontFamily: 'Baloo'
-      },
-      photoOptionsWrap: {
-        flexDirection:'row',
-        justifyContent:'space-around',
-       
-        marginVertical: 10,
-        position: 'absolute',
-        bottom: 30,
-        right: windowWidth/2 - 55
-      },
-      photoButton:{
-        justifyContent:'center',
-        alignItems:'center'
-      },
-      photoProfileIcon:{
-          width:100,
-          height:100,
-          borderRadius:60
-      },
+  upperContent: {
+    width: '100%',
+    height: windowHeigth * .2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    padding: 20
+  },
+  title: {
+    alignSelf: 'flex-start',
+    fontSize: 24,
+    fontFamily: 'Baloo'
+  },
+  tambahFotoBtn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  photoProfileIcon: {
+    width: 100,
+    height: 100,
+    borderRadius: 60
+  },
+  tambahFotoBtnTxt: {
+    fontFamily: 'QuickSand',
+    fontSize: 20
+  }
 })
