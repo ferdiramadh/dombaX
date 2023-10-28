d7707ae464ceb86bd11d5deb3c470a2b9fa21875
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-10-28 14:25:07 +0700

    GEMBUL-69 Some updates and clean up onboardingscreen.

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
 import Onboarding from 'react-native-onboarding-swiper'
 
 const OnboardingScreen = ({ navigation }) => {
-
-    const CustomImageContainer = (props) => {
-        return (
-            <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
-                <Text style={styles.titleOnboarding}>{props.title}</Text>
-                <Image source={props.img} style={props.styleImg} />
-            </View>
-        )
-    }
 
     const CustomOnDone = () => {
         return (
             <TouchableOpacity onPress={() => {
                 navigation.navigate('Home')
 
-            }} style={{ width: 100, height: 40, backgroundColor: '#ED9B83', justifyContent: 'center', alignItems: 'center', borderRadius: 10, marginHorizontal: 5 }}>
-                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#FFF', fontFamily: 'Baloo' }}>Masuk</Text>
+            }} style={styles.doneBtn}>
+                <Text style={styles.doneBtnTxt}>Masuk</Text>
             </TouchableOpacity>
         )
     }
@@ -32,21 +23,24 @@ const OnboardingScreen = ({ navigation }) => {
                     image: <Image source={require('../assets/images/onboard_1d.png')} style={{ width: 420, height: 420 }} />,
                     title: 'Kelola Stok',
                     subtitle: 'Ketahui stok produk dan atur setiap unit peternakan domba Kamu sebanyak apapun dari satu tempat.',
-                    titleStyles: { marginTop: -50 }
+                    titleStyles: { marginTop: -50 },
+                    subTitleStyles: styles.subTitle
                 },
                 {
                     backgroundColor: '#fff',
                     image: <Image source={require('../assets/images/aturtransaksinewest.png')} style={{ width: 280, height: 280 }} resizeMode='contain' />,
                     title: 'Atur Transaksi',
                     subtitle: 'Keuangan lebih rapi dan profesional tidak ada yang tercecer lagi.',
-                    titleStyles: { fontSize: 38, marginTop: -50 }
+                    titleStyles: { fontSize: 38, marginTop: -50 },
+                    subTitleStyles: styles.subTitle
                 },
                 {
                     backgroundColor: '#fff',
                     image: <Image source={require('../assets/images/LaporanInteraktif.png')} />,
                     title: 'Laporan Interaktif',
                     subtitle: 'Lihat laporan arus kas, untung/rugi, dan informasi finansial lainnya dalam satu dashboard interaktif. ',
-                    titleStyles: { fontSize: 38, marginTop: -50 }
+                    titleStyles: { fontSize: 38, marginTop: -50 },
+                    subTitleStyles: styles.subTitle
                 }
             ]}
             containerStyles={{ backgroundColor: '#fff', paddingHorizontal: 20 }}
@@ -72,5 +66,24 @@ const styles = StyleSheet.create({
     titleOnboarding: {
         fontFamily: 'Baloo-ExtraBold',
         fontSize: 50,
+    },
+    doneBtn: {
+        width: 100,
+        height: 40,
+        backgroundColor: '#ED9B83',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginHorizontal: 5
+    },
+    doneBtnTxt: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#FFF',
+        fontFamily: 'Baloo'
+    },
+    subTitle: { 
+        fontFamily: 'QuickSand', 
+        fontSize: 20
     }
 })
