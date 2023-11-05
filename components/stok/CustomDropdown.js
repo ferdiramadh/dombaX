ee9deaae8a821f178c1cef169d90746ae13ba19d
Ferdiansyah Ramadhan <ferdiansyah@jawasoft.com>
2023-11-05 17:39:48 +0700

    GEMBUL-72 Develop custom dropdown.

---
+++ components/stok/CustomDropdown.js
@@ -0,0 +1,89 @@
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import SelectCategoryModal from '../InventoryComponents/GlobalEditScreen/SelectCategoryModal'
+
+const CustomDropdown = ({ openFunc, title, data, setSelectedProduct, setFieldValue }) => {
+
+    const { isOpen, setIsOpen } = openFunc
+    const parentObj = { data, setSelectedProduct, setIsOpen, setFieldValue }
+
+    return (
+        <View style={styles.pickerContainer}>
+            <TouchableOpacity style={styles.item} onPress={() => setIsOpen(!isOpen)}>
+                <Text style={styles.title}>{title}</Text>
+                <MaterialIcons name={`keyboard-arrow-${isOpen ? 'up' : 'down'}`} size={24} color="black" />
+            </TouchableOpacity>
+            {isOpen && <DataWrapper parentObj={parentObj} />}
+        </View>
+    )
+}
+
+const DataWrapper = ({ parentObj }) => {
+
+    const [modalCategoryVisible, setModalCategoryVisible] = useState(false)
+    const [category, setCategory] = useState("Kategori")
+    const { data, setSelectedProduct, setIsOpen, setFieldValue } = parentObj
+
+    return (
+        <View style={styles.dataWrapper}>
+            <ScrollView>
+                {
+                    data.map((item, i) => {
+                        return <Item item={item} setSelectedProduct={setSelectedProduct} setIsOpen={setIsOpen} />
+                    })
+                }
+                <Item item={{ label: "Tambah Kategori Baru" }} onPress={() => setModalCategoryVisible(!modalCategoryVisible)} />
+            </ScrollView>
+            <SelectCategoryModal modalCategoryVisible={modalCategoryVisible} setModalCategoryVisible={setModalCategoryVisible} setFieldValue={setFieldValue} setCategory={setCategory} />
+        </View>
+    )
+}
+
+const Item = ({ item, setSelectedProduct, setIsOpen, onPress }) => {
+    return (
+        <TouchableOpacity style={styles.item} onPress={() => {
+            if (item.label !== "Tambah Kategori Baru") {
+                setSelectedProduct(item.value)
+                setIsOpen(false)
+            } else  {
+                onPress()
+            }
+        }}>
+            <Text style={styles.title}>{item.label}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default CustomDropdown
+
+const styles = StyleSheet.create({
+    pickerContainer: {
+        backgroundColor: '#DFE1E0',
+        width: '90%',
+
+        justifyContent: 'center',
+
+        marginVertical: 10,
+        borderRadius: 5
+    },
+    title: {
+        fontFamily: 'Quicksand',
+        color: "#474747",
+        fontSize: 14
+    },
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        // backgroundColor: 'yellow'
+    },
+    dataWrapper: {
+        // backgroundColor: 'red',
+        flex: 1,
+        maxHeight: 200,
+        marginTop: 10
+    }
+})
